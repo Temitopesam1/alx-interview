a8795706e8237529008932f71b24e5a703d32d23
Temitopesam1 <temitopesam5@gmail.com>
2023-04-26 15:00:20 +0000

    Feat: Explore Python in an NQUEENS project

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,8 +5,14 @@ import sys
 
 
 solutions = []
-board_size = 0
-position = None
+"""The list of possible solutions to the N queens problem.
+"""
+n = 0
+"""The size of the chessboard.
+"""
+pos = None
+"""The list of possible positions on the chessboard.
+"""
 
 
 def get_input():
@@ -15,35 +21,35 @@ def get_input():
     Returns:
         int: The size of the chessboard.
     """
-    global board_size
-    board_size = 0
+    global n
+    n = 0
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        board_size = int(sys.argv[1])
+        n = int(sys.argv[1])
     except Exception:
         print("N must be a number")
         sys.exit(1)
-    if board_size < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return board_size
+    return n
 
 
-def is_attacking(pos_0, pos_1):
+def is_attacking(pos0, pos1):
     """Checks if the positions of two queens are in an attacking mode.
 
     Args:
-        pos_0 (list or tuple): The first queen's position.
-        pos_1 (list or tuple): The second queen's position.
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
 
     Returns:
         bool: True if the queens are in an attacking position else False.
     """
-    if (pos_0[0] == pos_1[0]) or (pos_0[1] == pos_1[1]):
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
-    return abs(pos_0[0] - pos_1[0]) == abs(pos_0[1] - pos_1[1])
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
 
 def group_exists(group):
@@ -62,7 +68,7 @@ def group_exists(group):
             for grp_pos in group:
                 if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
                     i += 1
-        if i == board_size:
+        if i == n:
             return True
     return False
 
@@ -75,19 +81,16 @@ def build_solution(row, group):
         group (list of lists of integers): The group of valid positions.
     """
     global solutions
-    global board_size
-    if row == board_size:
+    global n
+    if row == n:
         tmp0 = group.copy()
         if not group_exists(tmp0):
             solutions.append(tmp0)
     else:
-        for col in range(board_size):
-            a = (row * board_size) + col
+        for col in range(n):
+            a = (row * n) + col
             matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(
-                                 lambda x: is_attacking(x[0], x[1]),
-                                 matches
-                             )
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
             group.append(pos[a].copy())
             if not any(used_positions):
                 build_solution(row + 1, group)
@@ -97,19 +100,14 @@ def build_solution(row, group):
 def get_solutions():
     """Gets the solutions for the given chessboard size.
     """
-    global position, board_size
-    position = list(
-                    map(
-                        lambda x: [x // board_size, x % board_size],
-                        range(board_size ** 2)
-                    )
-              )
+    global pos, n
+    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
     a = 0
     group = []
     build_solution(a, group)
 
 
-board_size = get_input()
+n = get_input()
 get_solutions()
 for solution in solutions:
     print(solution)
