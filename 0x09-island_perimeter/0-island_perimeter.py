7afb765ac993df83886b102099bde4b2fe623ebe
Temitopesam1 <temitopesam5@gmail.com>
2023-05-25 11:23:33 +0000

    FEAT: Explore Python basic concepts with getting perimeter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Island perimeter module
+'''
+
+
+def island_perimeter(grid):
+    '''Function to get an island perimeter
+    Args:
+        grid: A list of lists
+    returns:
+        perimetor or None if error
+    '''
+    grid_dup = []
+    length = 0
+    breadth = 0
+
+    for item in grid:
+        if 1 in item:
+            grid_dup.append(item)
+    if len(grid_dup) == 0:
+        return None
+    for i in range(len(grid_dup)):
+        grid_dup[i] = grid_dup[i][1:-1]
+    get_length = sorted(grid_dup, reverse=True)
+    for item in get_length[0]:
+        if item == 1:
+            length += item
+    for item in get_length:
+        breadth += item[0]
+    return 2 * (length + breadth)
