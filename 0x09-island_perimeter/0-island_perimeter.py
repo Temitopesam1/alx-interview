5606df51e455be60b5cbd87ac8b58102317751ff
Temi Sam <temitopesam5@gmail.com>
2023-05-26 08:53:16 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,21 +10,28 @@ def island_perimeter(grid):
     returns:
         perimetor or None if error
     '''
-    grid_dup = []
-    length = 0
-    breadth = 0
+#     grid_dup = []
+#     length = 0
+#     breadth = 0
 
-    for item in grid:
-        if 1 in item:
-            grid_dup.append(item)
-    if len(grid_dup) == 0:
-        return None
-    for i in range(len(grid_dup)):
-        grid_dup[i] = grid_dup[i][1:-1]
-    get_length = sorted(grid_dup, reverse=True)
-    for item in get_length[0]:
-        if item == 1:
-            length += item
-    for item in get_length:
-        breadth += item[0]
-    return 2 * (length + breadth)
+#     for item in grid:
+#         if 1 in item:
+#             grid_dup.append(item)
+#     if len(grid_dup) == 0:
+#         return None
+#     for i in range(len(grid_dup)):
+#         grid_dup[i] = grid_dup[i][1:-1]
+#     get_length = sorted(grid_dup, reverse=True)
+#     for item in get_length[0]:
+#         if item == 1:
+#             length += item
+#     for item in get_length:
+#         breadth += item[0]
+#     return 2 * (length + breadth)
+    count = 0
+    for elem in grind:
+        for item in elem:
+            if item == 1:
+                counter += item
+    counter += 1
+    return 2 * counter
